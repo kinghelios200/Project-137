b71015499b0146dd9496b86b3bf0eb04adb6a9b9
kinghelios200 <103470405+kinghelios200@users.noreply.github.com>
2023-01-10 18:52:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+
+function TV(){
+    window.location = "TV.html";
+}
+
+function gamingRoom(){
+    window.location = "GM.html";
+}
+
+function kitchen(){
+    window.location = "kitchen.html";
+}
+
+function livingRoom(){
+    window.location = "LV.html";
+}
